01ec74edd4ea5632120b6ae68b048ccfb58482ec
jhjguxin <864248765@qq.com>
2012-02-02 18:16:15 +0800

    add change order to user until the erp be closed,add summary for oders list view

--- apps/fanfou/views.py
+++ apps/fanfou/views.py
@@ -17,28 +17,31 @@ import calendar
 import pdb
 
 def placeOrder(request):
-    switch = False
+    late = False
+    todays_orders = False
     #pdb.set_trace()
     if request.user.is_anonymous():
         #return redirect_to_login('/',login_url="/login")
         return HttpResponseRedirect(urlreverse('login'))
     else:
-        #pdb.set_trace()#this time switch
+        #pdb.set_trace()#this time late
         d=datetime.date.today()
         #read today's menue if you want render the aritcle with mult category your can query category and send to templates
         today_article=Article.objects.filter(category__status=1)
         if datetime.datetime.now()>datetime.datetime(d.year,d.month, d.day, 10, 25, 0,0):
-            switch=True
+            late=True
             # if lunch was already submitted today...
         todays_orders = Traded_article.objects.filter(author=request.user,created_on__year=d.year,created_on__month=d.month,created_on__day=d.day)
         #pdb.set_trace()
         if todays_orders.exists():
-            switch = True
+            todays_orders = True
 
- 
+        if request.method == 'POST' and (todays_orders and not late):
+            Traded_article.objects.filter(author=request.user,created_on__year=d.year,created_on__month=d.month,created_on__day=d.day).delete()
+            return HttpResponseRedirect(urlreverse('f_order'))
             
     
-        if request.method == 'POST' and not switch: # If the form has been submitted...
+        if request.method == 'POST' and not late: # If the form has been submitted...
             #request.META['REMOTE_HOST']
             #pdb.set_trace()
 
@@ -57,28 +60,70 @@ def placeOrder(request):
             form = Traded_articleForm() # An unbound form
 
         return render_to_response('fanfou/orderform.html',
-                               {'form': form, 'username' : request.user.username, 'switch': switch,'articles':today_article },
+                               {'form': form, 'username' : request.user.username, 'late': late,"todays_orders":todays_orders,'articles':today_article },
                               context_instance=RequestContext(request))
 
 def thanks(request):
   return render_to_response("meal/thanks.html",{ 'user': request.user })
 
-def viewOrders(request, date=None):
+def viewOrders(request, date=None,summary=None):
     #pdb.set_trace()
-    if date=='today':  date = datetime.datetime.now()
+    if request.GET.has_key('date'):
+      if request.GET['date']==u'today':  date = datetime.datetime.now()
+    #if date=='today':  date = datetime.datetime.now()
+    if request.GET.has_key('summary'):
+      if request.GET['summary']==u'true':  summary=True
+
     if date:
         orders = Traded_article.objects.filter(created_on__year=date.year,created_on__month=date.month,created_on__day=date.day).all().order_by("created_on").reverse()
+        if summary:
+            orders=articleorder(orders)
     else:
         #orders = Traded_article.objects.all()
         orders = Traded_article.objects.all().order_by("created_on").reverse()
+        if summary:
+            orders=articleorder(orders)
     #total = reduce(lambda x,y: 1,orders,0)
     #pdb.set_trace()
     atotal=0
     for order in orders:
-      #pdb.set_trace()
-      atotal += order.total
+        #pdb.set_trace()
+        atotal += order.total
+
+
+
     return render_to_response('fanfou/vieworders.html',{'orders':orders, 'atotal':atotal,'user': request.user})
 
+def articleorder(orders=Traded_article.objects.all()):
+    #pdb.set_trace()
+    orders_list=list(orders.order_by("created_on").reverse())
+    orders_list1=list(orders.order_by("created_on").reverse())
+    #old_order=orders[0]
+    new_orders=[]
+    for old_order in orders_list:
+        #pdb.set_trace()
+        for d in orders_list1:
+            d.author.username='summaryshow'
+            if (d.article_id==old_order.article_id) and (d.id != old_order.id):
+                #pdb.set_trace()
+
+                d.guests=d.guests+(1+old_order.guests)
+                d.total= d.article.price*(d.guests+1)
+                try :
+                    orders_list1.remove(old_order)
+                except:
+                    pass
+
+
+
+        #old_order.total= old_order.article.price*(old_order.guests+1)
+        #new_orders.append(old_order)
+        #orders_list.remove(old_order)        
+
+
+#pdb.set_trace()
+    orders_list1.reverse()
+    return orders_list1
 
 def viewOrdersSummary(request, year=None, month=None):
     year = int(year)  if year is not None else  datetime.datetime.now().year
